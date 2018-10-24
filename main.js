dce925e4254854d18574d2b909211993fd87331d
Arthur Fanti <arthur.fanti@blinks.com.br>
2018-10-24 19:39:09 -0200

    mais um coomit depois do teste

--- main.js
+++ main.js
@@ -2,8 +2,4 @@ export const helloGit = () => "Hello Git asdadadsadasd"
 
 export const helloChuchu = () => "Hello Chuchu"
 
-<<<<<<< HEAD
 constante.apply(helloGit)
-=======
-export const helloChuchu = () => "Hello World"
->>>>>>> da9c38552731d6dd28580e327d15e05189965137
