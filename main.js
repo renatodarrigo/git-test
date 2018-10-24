5c849c533d7f6b795ceeeba0d14a7495e854b247
Marcio Bonaldo <marcio.bonaldo@blinks.com.br>
2018-10-24 18:56:45 -0300

    Atualização 2 Bonaldo

--- main.js
+++ main.js
@@ -5,5 +5,6 @@ export const helloChuchu = () => "Hello Chuchu"
 
 export const helloHi = () => "Hello World"
 export const helloHi = () => "Hello World builded by Chuchu"
+export const helloHi2 = () => "Hello World builded by Chuchu"
 
 constante.apply(helloGit)
